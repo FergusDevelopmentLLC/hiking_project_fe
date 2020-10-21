f4a1f1219639d93b78ed14b23105c278e1763190
Will Carter <will.carter@fergusllc.com>
2020-10-21 13:14:30 -0600

    bug fixing, api doesnt work after https

--- public/js/custom.js
+++ public/js/custom.js
@@ -251,9 +251,13 @@ displayTrailsByLatLng = async (e) => {
   let lat = e.lngLat.lat
   let apiUrl = `${url_prefix}/trails/${lat}/${lng}/5/15`
 
+  console.log('apiUrl', apiUrl)
+
   setSpinnerVisibilityTo('visible')
   let mapData = await fetch(apiUrl).then(r => r.json())
   setSpinnerVisibilityTo('hidden')
+
+  console.log('mapData', mapData)
 
   trailsArray = getTrailObjectsFrom(mapData)
   
